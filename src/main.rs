d82140e3a97e0bcb090258c7a75f009b0463423e
Lukas Weber <devlukas@hotmail.com>
2023-12-05 18:42:51 +0100

    Add ppm export for projectile simulation

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,37 @@
+use std::{fs::File, io::BufWriter};
+
 use raytracer::tuple::Tuple;
+
+use crate::raytracer::{canvas::Canvas, color::Color, exporter::{PPMExporter, Exporter}};
 
 mod raytracer;
 
 fn main() {
-    let mut p = Projectile { position: Tuple::point(0.0, 1.0, 0.0), velocity: Tuple::vector(1.0, 1.0, 0.0).normalize() };
+    let mut p = Projectile { position: Tuple::point(0.0, 1.0, 0.0), velocity: Tuple::vector(1.0, 1.8, 0.0).normalize() * 11.25 };
     let e = Environment { gravity: Tuple::vector(0.0, -0.1, 0.0), wind: Tuple::vector(-0.01, 0.0, 0.0) };
+    let mut c = Canvas::new(900, 550);
+
     println!("{}", p.position);
     while p.position.y() > 0.0 {
         p = tick(&e, p);
+        let target_x = p.position.x().round() as usize;
+        let target_y = c.height() - (p.position.y().round() as usize);
+        if target_y < c.height() && target_x < c.width() {
+            c.write_pixel(target_x, target_y, Color::new(255.0, 0.0, 0.0));
+        }
         println!("{}", p.position);
     }
+    let exporter = PPMExporter::new();
+    let file= File::create("out.ppm").unwrap();
+    let mut writer = BufWriter::new(file);
+
+    use std::time::Instant;
+    let now = Instant::now();
+    
+    exporter.export(&c, &mut writer).unwrap();
+
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
 }
 
 struct Projectile {
